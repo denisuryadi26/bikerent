2cc1350a9d76751fe18044104e6317525e6cc01f
denisuryadi26 <deni.w4f@gmail.com>
2021-02-11 19:51:47 +0700

    Bikerent Project

---
+++ resources/views/kategori/create.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Category Management</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('kategori.index') }}">Category</a></li>
+                        <li class="breadcrumb-item active">Create Category</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class=" d-flex align-items-center justify-content-between">
+                            <a href="{{ route('kategori.index')}}" class="btn">
+                                <i class="fas fa-arrow-left  text-purple  "></i>
+                            </a>
+                            <span>Add Category Form</span>
+                        </div>
+
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body" >
+                        <div class="row">
+                            <div class="col-md-6 mx-auto">
+                                <form method="POST" action="{{ route('kategori.store') }}">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label for="nama_kategori">Category Name</label>
+                                        <input type="text" name="nama_kategori" class="form-control @error('nama_kategori') is-invalid @enderror"  id="nama_kategori" value="{{ old('nama_kategori') }}"  placeholder="Masukkan Nama Kategori" autofocus>
+                                        @error('nama_kategori')
+                                            <div class="text-danger small mt-1">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    {{-- <hr> --}}
+                                    <div class="form-group d-flex justify-content-end">
+                                        <a class="btn btn-default " href="{{ route('kategori.index') }}">Cancel</a>
+                                        <button type="submit" class="btn btn-primary ml-2">
+                                            Save
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                    {{-- <div class="card-footer clearfix">
+                        tes
+                    </div> --}}
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+
+    </section>
+    <!-- /.content -->
+</div>
+@endsection
+
+@section('scripts')
+    @if(session()->has('success'))
+    <script>
+        $(document).ready(function () {
+            toastr["success"]('{{ session()->get('success') }}')
+        });
+
+    </script>
+    @endif
+@endsection
